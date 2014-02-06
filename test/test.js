8934ffdf4e0765508d13742a66334466743660ed
mccarthy527 <mccarthy527@gmail.com>
2014-02-06 13:59:22 -0600

    fixed test cases

--- test/test.js
+++ test/test.js
@@ -1,58 +1,33 @@
 var fs = require("fs")
 var gc = require("../index.js")
 var dp = require("../doparse.js")
+var test = require('tape')
+var path = require('path')
 
-problem=2
-var data
-
-if(problem==1)
-{
-	data = fs.readFileSync("test/simpletest1.gcode")
-}
-else if(problem==2)
-{
-	data = fs.readFileSync("test/simpletest2.gcode")
-}
-else if(problem==3)
-{
-	data = fs.readFileSync("test/text.gcode")
+function runTest(t, fileName) {
+	var data = fs.readFileSync(path.join(__dirname, fileName))
+	var fileContent = data.toString()
+	var history = [[]]
+	t.doesNotThrow(function() {
+		history = gc(fileContent)
+	})
+	return history
 }
 
-
-var fileContent = data.toString()
-
-history = gc(fileContent)
-console.log(history)
-console.log(history.length)
-
-
-if(problem==1)
+function testcase(t)
 {
-	laststate = history[history.length-1];
-	
-	if(!((laststate.x[0] == 98.132) && (laststate.x[0] == 98.132) && (laststate.x[0] == 98.132) && (laststate.e == 0.9889899999999999) && (laststate.f == 600) && (laststate.erel == -0.28188) && (laststate.xrel[0] == 0)))
-	{
-		throw new Error("failed test case")
-	}
-	else
-	{
-		console.log('passed test!')
-	}
-}
-else if(problem==2)
-{
-	secondtolaststate = history[history.length-2];
-	if(secondtolaststate.fp == -0.5)
-	{
-		console.log('passed test!')
-	}
-	else
-	{
-		throw new Error("failed test case")
-	}
-}
+	var history = runTest(t, "simpletest1.gcode")
+	var laststate = history[history.length-1];
+	t.equal(laststate.x[0],98.132)
+	t.equal(laststate.e, 0.9889899999999999)
+	t.equal(laststate.f,600)
+	t.equal(laststate.erel, -0.28188)
+	t.equal(laststate.xrel[0],0)
 
-/*
-console.log(gcode[1])
-console.log(gc.getValue(gcode[1], 'E'))	//I don't know how to set the file up to allow me to do this
-*/
+	var history = runTest(t, "simpletest2.gcode")
+	var secondtolaststate = history[history.length-2]
+	t.equals(secondtolaststate.fp, -0.5)
+
+	t.end()
+}
+test('test1', testcase)
