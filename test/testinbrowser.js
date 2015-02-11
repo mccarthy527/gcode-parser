e9d67444f6f7387b33316ba75b2f019e1582cf54
Brian McCarthy <mccarthy527@gmail.com>
2015-02-11 15:26:51 -0600

    not sure

--- test/testinbrowser.js
+++ test/testinbrowser.js
@@ -1,11 +1,10 @@
-var fs = require("fs")
 var gc = require("../index.js")
 //var test = require('tape')
 //var path = require('path')
 
 
-var data = fs.readFileSync("simpletest1.gcode")
-var fileContent = data.toString()
+
+var fileContent = "G1 Z0.200 F7800.000\nG1 X92.960 Y92.960"
 
 history = gc(fileContent)
 console.log(history)
